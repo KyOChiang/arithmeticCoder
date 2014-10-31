ed3e503158559854d6724130c5e5137a6f649a3e
Soo Fatt <soofatt93@gmail.com>
2014-10-31 11:00:00 +0800

    Add lengthy test case.

--- test/test_IT_arithmeticCoder.c
+++ test/test_IT_arithmeticCoder.c
@@ -8,6 +8,7 @@ void setUp(void){}
 void tearDown(void){}
 
 void test_cftNew_should_create_cft_for_symbols_from_file(){
+  int i;
   Stream *in = NULL;
   CFT *cftPtr;
   int tableSize;
@@ -15,28 +16,29 @@ void test_cftNew_should_create_cft_for_symbols_from_file(){
   
   Try{
   // Please check cftData.txt, inside has a lengthy string 
-    in = openStream("cftData.txt","r");
+    in = openStream("lengthy.txt","rb");
     cftPtr = cftNew(in, &tableSize);
   }Catch(error){
     TEST_ASSERT_NOT_EQUAL(ERR_FILE_NOT_EXIST,error);
   }
-  dumpCFT(cftPtr,0);
-  dumpCFT(cftPtr,1);
-  dumpCFT(cftPtr,2);
+  
+  for(i = 0; i <= 255; i++){
+  dumpCFT(cftPtr,i);}
+  
   
   TEST_ASSERT_EQUAL(3,tableSize);
   
-  TEST_ASSERT_EQUAL('a', cftPtr[0].symbol);
-  TEST_ASSERT_EQUAL(40, cftPtr[0].cum_Freq);
-  TEST_ASSERT_EQUAL(40, cftPtr[0].occurNo);
+  // TEST_ASSERT_EQUAL('a', cftPtr[0].symbol);
+  // TEST_ASSERT_EQUAL(40, cftPtr[0].cum_Freq);
+  // TEST_ASSERT_EQUAL(40, cftPtr[0].occurNo);
 
-  TEST_ASSERT_EQUAL('b', cftPtr[1].symbol);
-  TEST_ASSERT_EQUAL(41, cftPtr[1].cum_Freq);
-  TEST_ASSERT_EQUAL(1, cftPtr[1].occurNo);
+  // TEST_ASSERT_EQUAL('b', cftPtr[1].symbol);
+  // TEST_ASSERT_EQUAL(41, cftPtr[1].cum_Freq);
+  // TEST_ASSERT_EQUAL(1, cftPtr[1].occurNo);
 
-  TEST_ASSERT_EQUAL('c', cftPtr[2].symbol);
-  TEST_ASSERT_EQUAL(50, cftPtr[2].cum_Freq);
-  TEST_ASSERT_EQUAL(9, cftPtr[2].occurNo);
+  // TEST_ASSERT_EQUAL('c', cftPtr[2].symbol);
+  // TEST_ASSERT_EQUAL(50, cftPtr[2].cum_Freq);
+  // TEST_ASSERT_EQUAL(9, cftPtr[2].occurNo);
   
   if(in != NULL){
     closeStream(in);
@@ -51,9 +53,9 @@ void test_arithmeticEncoder_to_encode_acba_with_cftData_txt_should_generate_a_ta
   
   Try{
   // Please check cftData.txt, inside has a lengthy string 
-    cft = openStream("cftData.txt","r");
+    cft = openStream("lengthy.txt","rb");
     cftPtr = cftNew(cft,&tableSize);
-    TEST_ASSERT_EQUAL(3,tableSize);
+    TEST_ASSERT_EQUAL(93,tableSize);
   }Catch(error){
     TEST_ASSERT_NOT_EQUAL(ERR_FILE_NOT_EXIST,error);
   }
@@ -67,13 +69,13 @@ void test_arithmeticEncoder_to_encode_acba_with_cftData_txt_should_generate_a_ta
    * To see the final result, using a hex editor to open the tag file and compare with
    * print statement at streamFlush.
    */
-    in = openStream("encodeACBA.txt","r");
-    out = openStream("tag.bin","w");
+    in = openStream("lengthy.txt","rb");
+    out = openStream("tag.bin","wb");
     arithmeticEncode(in, &dataLength, cftPtr,tableSize,out);
   }Catch(error){
     TEST_ASSERT_NOT_EQUAL(ERR_FILE_NOT_EXIST,error);
   }
-  TEST_ASSERT_EQUAL(4,dataLength);
+  TEST_ASSERT_EQUAL(15265,dataLength);
   if(in != NULL){
     closeStream(in);
   }if(out != NULL){
@@ -83,35 +85,35 @@ void test_arithmeticEncoder_to_encode_acba_with_cftData_txt_should_generate_a_ta
   }
 }
 
-void test_tagReader_should_read_in_32bit_of_tag_value(){
-  Stream *in = NULL;
-  uint32 tag = 0;
-  CEXCEPTION_T error;
+// void test_tagReader_should_read_in_32bit_of_tag_value(){
+  // Stream *in = NULL;
+  // uint32 tag = 0;
+  // CEXCEPTION_T error;
   
-  Try{
-    in = openStream("tag.bin","r");
-    tagReader(in,&tag);
-  }Catch(error){
-    TEST_ASSERT_NOT_EQUAL(ERR_FILE_NOT_EXIST,error);
-  }
-  TEST_ASSERT_EQUAL(0xC56D5CF9,tag);
-  if(in != NULL){
-    closeStream(in);
-  }
-}
+  // Try{
+    // in = openStream("tag.bin","r");
+    // tagReader(in,&tag);
+  // }Catch(error){
+    // TEST_ASSERT_NOT_EQUAL(ERR_FILE_NOT_EXIST,error);
+  // }
+  // TEST_ASSERT_EQUAL(0xC56D5CF9,tag);
+  // if(in != NULL){
+    // closeStream(in);
+  // }
+// }
 
 void test_arithmeticDecoder_to_decode_tag_with_cftData_txt_should_generate_acba(){
   Stream *cft = NULL, *in = NULL, *out = NULL;
   CFT *cftPtr;
   uint32 tag = 0;
-  int tableSize, dataLength = 4;
+  int tableSize, dataLength = 15265;
   CEXCEPTION_T error;
   
   Try{
   // Please check cftData.txt, inside has a lengthy string 
-    cft = openStream("cftData.txt","r");
+    cft = openStream("lengthy.txt","rb");
     cftPtr = cftNew(cft,&tableSize);
-    TEST_ASSERT_EQUAL(3,tableSize);
+    TEST_ASSERT_EQUAL(93,tableSize);
   }Catch(error){
     TEST_ASSERT_NOT_EQUAL(ERR_FILE_NOT_EXIST,error);
   }
@@ -121,8 +123,8 @@ void test_arithmeticDecoder_to_decode_tag_with_cftData_txt_should_generate_acba(
    * tag.txt/.bin will store the tag generate from encode data
    * acba store into decodeSymbol.txt
    */
-    in = openStream("tag.bin","r");
-    out = openStream("decodedSymbol.txt","w");
+    in = openStream("tag.bin","rb");
+    out = openStream("decodedSymbol2.txt","wb");
     tagReader(in,&tag);
     arithmeticDecode(&dataLength, &tag, cftPtr, tableSize, out, in);
   }Catch(error){
